91b0fb3dfb62fcae8a3b957778ac366fd765cea9
Pichu Chen <pichubaby@gmail.com>
2016-04-11 01:05:00 +0800

    add Target User

---
+++ lbot/bot.go
@@ -0,0 +1,43 @@
+package lbot
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func (b *Bot) SetConfig(c Config) {
+	b.config = &c
+}
+
+func (b *Bot) SendTextMessage(mid, s string) error {
+	if b.config == nil {
+		return errors.New("Config have not been set")
+	}
+
+	var payload Request
+	payload.SetDefaults()
+	payload.SetText(s)
+	payload.AddTargetUser(mid)
+
+	req, err := http.NewRequest("POST", b.config.ServerHost+"/1/events", strings.NewReader(string(s)))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-type", "application/json;charset=UTF-8]")
+	req.Header.Set("X-Line-ChannelID", b.config.ChannelID)
+	req.Header.Set("X-Line-ChannelSecret", b.config.ChannelSecret)
+	req.Header.Set("X-Line-Trusted-User-With-ACL", b.config.MID)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
