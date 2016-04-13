439fa8b760f4310d755de47999e03f89b2e87d27
Pichu Chen <pichubaby@gmail.com>
2016-04-14 00:07:51 +0800

    update example

--- example/main.go
+++ example/main.go
@@ -38,10 +38,50 @@ func index(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	log.Printf("User %v: %v\n", req.Result[0].Content.From, req.Result[0].Content.Text)
-	receivedMsg := req.Result[0].Content.Text
 
-	log.Printf("%v\n", req)
-	bot.SendTextMessage(req.Result[0].Content.From, receivedMsg)
+	profiles, _ := bot.GetUserProfile(req.Result[0].Content.From)
+	if req.Result[0].EventType == lbot.EventOperation {
 
+	} else {
+		contentType := req.Result[0].Content.ContentType
+		switch contentType {
+		case lbot.TextMessage:
+			log.Printf("User %v -> Bot: %v\n", profiles[0].DisplayName, req.Result[0].Content.Text)
+			receivedMsg := req.Result[0].Content.Text
+			bot.SendTextMessage(req.Result[0].Content.From, receivedMsg)
+			log.Printf("Bot -> User %v: %v", profiles[0].DisplayName, receivedMsg)
+		case lbot.ImageMessage:
+			log.Printf("User %v -> Bot: Image id : %v\n", profiles[0].DisplayName, req.Result[0].Content.Id)
+		case lbot.VideoMessage:
+			log.Printf("User %v -> Bot: Video id : %v\n", profiles[0].DisplayName, req.Result[0].Content.Id)
+		case lbot.AudioMessage:
+			log.Printf("User %v -> Bot: Audio id : %v\n", profiles[0].DisplayName, req.Result[0].Content.Id)
+		case lbot.LocationMessage:
+			log.Printf("User %v -> Bot: Location %v %v (%v, %v)\n", profiles[0].DisplayName,
+				req.Result[0].Content.Location.Title,
+				req.Result[0].Content.Location.Address,
+				req.Result[0].Content.Location.Latitude,
+				req.Result[0].Content.Location.Longitude)
+		case lbot.StickerMessage:
+			log.Printf("User %v -> Bot: Stiker  %v\n", profiles[0].DisplayName,
+				buildStickerUrl(
+					lbot.Sticker{
+						Stkver:   req.Result[0].Content.ContentMetadata.Stkver,
+						Stkpkgid: req.Result[0].Content.ContentMetadata.Stkpkgid,
+						Stkid:    req.Result[0].Content.ContentMetadata.Stkid}))
+
+		default:
+			log.Printf("Unsupport content type: %v from %v", contentType, req.Result[0].Content.From)
+
+		}
+	}
+
+	// log.Printf("User %v: %v\n", req.Result[0].Content.From, req.Result[0].Content.Text)
+
+	// log.Printf("%v: %v\n", profiles[0].DisplayName, req)
+
+}
+
+func buildStickerUrl(s lbot.Sticker) string {
+	return "http://dl.stickershop.line.naver.jp/products/0/0/" + s.Stkver + "/" + s.Stkpkgid + "/PC/stickers/" + s.Stkid + ".png"
 }
